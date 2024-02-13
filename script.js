3eaf3b4305f6ce8629b92ef758b718d137e599c1
Abhishek <abhishek.180thomas@gmail.com>
2024-02-13 10:23:49 +0530

    fixed the focus color bug with password comparison.

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const confirmPassword = document.querySelector("#confirm_password");
 const submitButton = document.querySelector(".create-account-btn");
 
 function validatePasswords() {
-    if (password.value !== confirmPassword.value) {
+
+    
+    if ((password.value !== confirmPassword.value) || 
+       (password.value == '' && confirmPassword.value == '')) {
         password.style.borderColor = 'red';
         confirmPassword.style.borderColor = 'red';
-    } else {
+    } 
+    else {
         password.style.borderColor = 'green';
         confirmPassword.style.borderColor = 'green';
     }
+
 }
 
+password.addEventListener("input", validatePasswords)
 confirmPassword.addEventListener("input", validatePasswords);
